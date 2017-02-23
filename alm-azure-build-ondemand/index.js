cf4cebd2dbbb46afb0f5b13e0ef397ea3f821044
EricoGR <ericogr@gmail.com>
2017-02-23 14:36:10 -0300

    chamar a função assincrona

--- alm-azure-build-ondemand/index.js
+++ alm-azure-build-ondemand/index.js
@@ -39,7 +39,7 @@ module.exports = function(context, data) {
     }
     else if("eventType" in data && data.eventType === "build.complete") {
 	     context.log("Iniciando desligamento da máquina virtual...");
-       AzureVirtualMachine.deallocate(parameters.azure)
+       AzureVirtualMachine.deallocateAsync(parameters.azure)
            .then((ret) => {
                context.log(`Máquina virtual ${parameters.azure.virtualMachine.vmName} desligada!`);
                return ret;
